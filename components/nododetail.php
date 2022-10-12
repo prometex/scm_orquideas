7a8f4dc69b529621e451afca7776ef811712ca06
cyborpaul <leowaldir2000@gmail.com>
2022-10-12 10:47:14 -0500

    holalll11444444444444444444444571411422561

--- components/nododetail.php
+++ components/nododetail.php
@@ -12,6 +12,7 @@ $sql = sprintf("SELECT * FROM sanmarcos_nodo;");
             'nod_int_id' => $rows['nod_int_id'],
             'nod_txt_description'=>$row['nod_txt_description'],
             'nod_txt_latitud' => $rows['nod_double_latitud'],
+            'nod_txt_longitud' => $rows['nod_double_longitud'],
             'nod_txt_name' => $rows['nod_txt_name']
 					);
 	}
