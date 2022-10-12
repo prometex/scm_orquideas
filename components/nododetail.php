f0744adf7e00b63402f638dfabe21ebf5c9da375
cyborpaul <leowaldir2000@gmail.com>
2022-10-12 10:52:26 -0500

    holalll114444444444444444444445714114225611111

--- components/nododetail.php
+++ components/nododetail.php
@@ -16,8 +16,8 @@ $sql = sprintf("SELECT * FROM sanmarcos_nodo;");
             'nod_txt_distrito' => $rows['nod_txt_distrito'],
             'nod_txt_provincia' => $rows['nod_txt_provincia'],
             'nod_txt_region'=> $rows['nod_txt_region'],
-            'nod_txt_provincia' => $rows['nod_txt_provincia'],
-            'nod_txt_region'=> $rows['nod_txt_region'],
+            'nod_txt_registro' => $rows['nod_date_registro'],
+            'nod_txt_actualizacion' => $rows['nod_date_actualizacion'],
             'nod_txt_name' => $rows['nod_txt_name']
 					);
 	}
@@ -44,7 +44,8 @@ while($rows = $query->fetch_array()) {
                 
 ,
                 'nod_txt_habilitado' => $rows['nod_int_habilitado'],
-x
+                'nod_txt_registro' => $rows['nod_date_registro'],
+                'nod_txt_actualizacion' => $rows['nod_date_actualizacion']
 
                 );
 }
