56e43812267d3e255b66c4138c2976d7b6eee8a8
cyborpaul <leowaldir2000@gmail.com>
2022-10-12 10:05:33 -0500

    holalll1144444444444444444444457

--- components/nododetail.php
+++ components/nododetail.php
@@ -9,7 +9,7 @@ $sql = sprintf("SELECT * FROM sanmarcos_nodo;");
 	while($rows = $query->fetch_array()) {
 		
 		$response []= array(
-
+            'nod_int_id' => $rows['nod_int_id'],
             'nod_txt_name' => $rows['nod_txt_name']
 					);
 	}
