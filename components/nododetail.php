bf0270a9ef8e41f03e7beefd9e1eba450903e12b
cyborpaul <leowaldir2000@gmail.com>
2022-10-12 09:40:27 -0500

    holalll11444444444444444

--- components/nododetail.php
+++ components/nododetail.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOZU</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php 
+
 require '../config.php';
 header('Content-Type: application/json');
 $sql=sprintf("SELECT * FROM sanmarcos_nodo");
